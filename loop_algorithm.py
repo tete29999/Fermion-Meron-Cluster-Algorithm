8f60a4a33279859925a08c3a6d48ea7fb0ce1a71
Thea Budde <theabudde@gmail.com>
2022-05-11 20:15:20 +0200

    full algorithm with bug
    always stays with vertical fermion config

--- loop_algorithm.py
+++ loop_algorithm.py
@@ -4,24 +4,19 @@ import numpy as np
 from itertools import product
 
 def main():
-    n = 10  # number of lattice points
-    t = 10   # number of half timesteps (#even + #odd)
-    mc_steps = 5   # number of mc steps
-    w_a = 0.2 # weight of a plaquettes
-    w_b = 0.8 # weight of b plaquettes
-    fermion = np.full((n, t), False) # fermion lattice
-    bond = np.full((n//2, t), False) # bond lattice, 0 is vertical plaquette A, 1 is horizontal plaquette B
+    n = 12  # number of lattice points
+    t = 12   # number of half timesteps (#even + #odd)
+    b = 1 # beta
+    mc_steps = 50000   # number of mc steps
+    w_a = np.cosh(b/t)  # weight of a plaquettes U = t = 1
+    w_b = np.sinh(b/t)  # weight of b plaquettes
+    fermion = np.full((n, t), False)    # fermion lattice
+    bond = np.full((n//2, t), False)    # bond lattice, 0 is vertical plaquette A, 1 is horizontal plaquette B
     Z = 0
 
     for mc in range(mc_steps):
-        # calculate weights
-        weight_factor = 1
-        for x, y in product(range(n//2), range(t)):
-            weight_factor *= w_a if not bond[x, y] else w_b
-        Z += weight_factor
-
         # find clusters and flip
-        visited = np.full((n, t), False) # record if site has been visited
+        visited = np.full((n, t), False)    # record if site has been visited
         x = 0
         y = 0
         while True:
@@ -29,27 +24,73 @@ def main():
             while True:
                 fermion[x, y] = 1 - fermion[x, y] if flip else fermion[x, y]
                 visited[x, y] = True
-                # upward bond
-                if not bond[x//2, (y - 1)%t] and not visited[x, (y - 1)%t]:
-                    y -= 1
-                # downward bond
-                elif not bond[x // 2, y] and not visited[x, (y + 1)%t]:
-                    y += 1
-                # left bond
-                elif (x % 2 == 0 and y % 2 == 0) or (x % 2 == 1 and y % 2 == 1) and bond[x//2, (y - 1)%t] and not visited[x - 1, y]:
-                    x -= 1
-                elif (x % 2 == 1 and y % 2 == 0) or (x % 2 == 1 and y % 2 == 0) and bond[x//2, y] and not visited[(x - 1)%n, y]:
-                    x -= 1
-                # right bond
-                elif (x % 2 == 0 and y % 2 == 0) or (x % 2 == 1 and y % 2 == 1) and bond[x//2, y] and not visited[(x + 1)%n, y]:
-                    x -= 1
-                elif (x % 2 == 1 and y % 2 == 0) or (x % 2 == 1 and y % 2 == 0) and bond[x // 2, (y - 1)%t] and not visited[(x + 1)%n, y]:
-                    x -= 1
-                # every existent bond visited, therefore loop closed
-                else:
-                    break
+                if x % 2 == 0 and y % 2 == 0:
+                    # top
+                    if not bond[(x//2 - 1)%(t//2), y - 1] and not visited[x, (y-1)%t]:
+                        y -= 1
+                    # left
+                    elif bond[(x//2 - 1)%(t//2), y - 1] and not visited[(x-1)%t, y]:
+                        x -= 1
+                    # bottom
+                    elif not bond[x//2, y] and not visited[x, (y+1)%t]:
+                        y += 1
+                    # right
+                    elif bond[x//2, y] and not visited[(x+1)%t, y]:
+                        x += 1
+                    # closed loop
+                    else:
+                        break
+                elif x % 2 == 1 and y % 2 == 0:
+                    # top
+                    if not bond[x//2, y - 1] and not visited[x, (y-1)%t]:
+                        y -= 1
+                    # left
+                    elif bond[x//2, y] and not visited[(x-1)%t, y]:
+                        x -= 1
+                    # bottom
+                    elif not bond[x//2, y] and not visited[x, (y+1)%t]:
+                        y += 1
+                    # right
+                    elif bond[x//2, y-1] and not visited[(x+1)%t, y]:
+                        x += 1
+                    # closed loop
+                    else:
+                        break
+                elif x % 2 == 0 and y % 2 == 1:
+                    # top
+                    if not bond[x//2, y - 1] and not visited[x, (y-1)%t]:
+                        y -= 1
+                    # left
+                    elif bond[(x//2 - 1)%(t//2), y] and not visited[(x-1)%t, y]:
+                        x -= 1
+                    # bottom
+                    elif not bond[(x//2 - 1)%(t//2), y] and not visited[x, (y+1)%t]:
+                        y += 1
+                    # right
+                    elif bond[x//2, y-1] and not visited[(x+1)%t, y]:
+                        x += 1
+                    # closed loop
+                    else:
+                        break
+                elif x % 2 == 1 and y % 2 == 1:
+                    # top
+                    if not bond[x//2, y - 1] and not visited[x, (y-1)%t]:
+                        y -= 1
+                    # left
+                    elif bond[x//2, y - 1] and not visited[(x-1)%t, y]:
+                        x -= 1
+                    # bottom
+                    elif not bond[x//2, y] and not visited[x, (y+1)%t]:
+                        y += 1
+                    # right
+                    elif bond[x//2, y] and not visited[(x+1)%t, y]:
+                        x += 1
+                    # closed loop
+                    else:
+                        break
                 x = x % n
                 y = y % t
+
             full = False
             for i, j in product(range(n), range(t)):
                 if not visited[i, j]:
@@ -60,10 +101,24 @@ def main():
                     full = True
             if full:
                 break
-        print(Z)
 
+        # cluster assignment
+        for x, y in product(range(0, n, 2), range(t)):
+            if fermion[x, y] == fermion[x, (y + 1) % t] == fermion[(x + 1) % n, y] == fermion[(x + 1) % n, (y + 1) % t]:
+                bond[x // 2, y] = False
+            if fermion[x, y] != fermion[x, (y + 1) % t]:
+                bond[x// 2, y] = True
+            else:
+                bond[x // 2, y] = False if random.random() < w_a/(w_a + w_b) else True
 
+        # calculate weights
+        weight_factor = 1
+        for x, y in product(range(n // 2), range(t)):
+            weight_factor *= w_a if not bond[x, y] else w_b
+        Z += weight_factor
+        #print(weight_factor)
 
+    print(Z)
 
 if __name__ == "__main__":
     main()
