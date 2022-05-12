dc6bd1dd13899f9f5bc92da4fb778c68cac6dfad
Thea Budde <theabudde@gmail.com>
2022-05-12 15:51:33 +0200

    Added comments

--- loop_algorithm.py
+++ loop_algorithm.py
@@ -1,5 +1,4 @@
 import random
-
 import numpy as np
 from itertools import product
 
@@ -10,7 +9,9 @@ def main():
     mc_steps = 5000   # number of mc steps
     w_a = np.cosh(b/t)  # weight of a plaquettes U = t = 1
     w_b = np.sinh(b/t)  # weight of b plaquettes
-    fermion = np.full((n, t), False)    # fermion lattice
+
+    fermion = np.full((n, t), False)    # fermion lattice initialized to empty
+    # bond lattice is squashed down and initalized to vertical plaquettes
     bond = np.full((n//2, t), False)    # bond lattice, 0 is vertical plaquette A, 1 is horizontal plaquette B
     Z = 0
 
@@ -23,10 +24,11 @@ def main():
         x = 0
         y = 0
         while True:
-            flip = 0 if random.random() < 0.5 else 1
+            flip = 0 if random.random() < 0.5 else 1 # decide if next cluster is to be flipped
             while True:
-                fermion[x, y] = 1 - fermion[x, y] if flip else fermion[x, y]
+                fermion[x, y] = 1 - fermion[x, y] if flip else fermion[x, y] # flip
                 visited[x, y] = True
+                # follow bond loop
                 if x % 2 == 0 and y % 2 == 0:
                     # top
                     if not bond[(x//2 - 1)%(n//2), (y - 1)%t] and not visited[x, (y-1)%t]:
@@ -91,7 +93,7 @@ def main():
                     # closed loop
                     else:
                         break
-                x = x % n
+                x = x % n # boundary conditions
                 y = y % t
 
             full = False
@@ -101,7 +103,7 @@ def main():
                     y = j
                     break
                 if i == n-1 and j == t-1:
-                    full = True
+                    full = True # if last indices are reached everything has been visited
             if full:
                 break
 
@@ -109,24 +111,25 @@ def main():
         for x, y in product(range(n), range(t)):
             if y % 2 != x % 2:
                 continue
+            # all occupied or all unoccupied
             if fermion[x, y] == fermion[(x + 1) % n, y] and fermion[x, (y + 1) % t] == fermion[(x + 1) % n, (y + 1) % t]:
                 bond[x // 2, y] = False
+            # diagonal occupation
             elif fermion[x, y] != fermion[x, (y + 1) % t]:
                 bond[x// 2, y] = True
+            # parallel occupation
             else:
                 bond[x // 2, y] = False if random.random() < w_a/(w_a + w_b) else True
             # calculate bond config in nicer lattice for debugging purposes
-            if y % 2 == 0:
-                bond_debug[x, y] = bond[x//2, y]
-            else:
-                bond_debug[x, y] = bond[x//2, y]
+            bond_debug[x, y] = bond[x // 2, y]
 
-        # calculate weights
+        # calculate weight of configuration
         weight_factor = 1
         for x, y in product(range(n // 2), range(t)):
             weight_factor *= w_a if not bond[x, y] else w_b
+
+        # add to total
         Z += weight_factor
-        #print(weight_factor)
 
     print(Z)
 
