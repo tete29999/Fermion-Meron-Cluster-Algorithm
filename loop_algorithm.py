79ee71510fea5d61997776562c4c224716029448
Thea Budde <theabudde@gmail.com>
2022-05-12 15:20:33 +0200

    corrected modulos

--- loop_algorithm.py
+++ loop_algorithm.py
@@ -4,15 +4,18 @@ import numpy as np
 from itertools import product
 
 def main():
-    n = 12  # number of lattice points
-    t = 12   # number of half timesteps (#even + #odd)
+    n = 8  # number of lattice points
+    t = 10   # number of half timesteps (#even + #odd)
     b = 1 # beta
-    mc_steps = 50000   # number of mc steps
+    mc_steps = 5000   # number of mc steps
     w_a = np.cosh(b/t)  # weight of a plaquettes U = t = 1
     w_b = np.sinh(b/t)  # weight of b plaquettes
     fermion = np.full((n, t), False)    # fermion lattice
     bond = np.full((n//2, t), False)    # bond lattice, 0 is vertical plaquette A, 1 is horizontal plaquette B
     Z = 0
+
+    #random.seed(42)
+    bond_debug = np.full((n, t), - 1) # only for debugging purposes
 
     for mc in range(mc_steps):
         # find clusters and flip
@@ -26,64 +29,64 @@ def main():
                 visited[x, y] = True
                 if x % 2 == 0 and y % 2 == 0:
                     # top
-                    if not bond[(x//2 - 1)%(t//2), y - 1] and not visited[x, (y-1)%t]:
+                    if not bond[(x//2 - 1)%(n//2), (y - 1)%t] and not visited[x, (y-1)%t]:
                         y -= 1
                     # left
-                    elif bond[(x//2 - 1)%(t//2), y - 1] and not visited[(x-1)%t, y]:
+                    elif bond[(x//2 - 1)%(n//2), (y - 1)%t] and not visited[(x-1)%n, y]:
                         x -= 1
                     # bottom
                     elif not bond[x//2, y] and not visited[x, (y+1)%t]:
                         y += 1
                     # right
-                    elif bond[x//2, y] and not visited[(x+1)%t, y]:
+                    elif bond[x//2, y] and not visited[(x+1)%n, y]:
                         x += 1
                     # closed loop
                     else:
                         break
                 elif x % 2 == 1 and y % 2 == 0:
                     # top
-                    if not bond[x//2, y - 1] and not visited[x, (y-1)%t]:
+                    if not bond[x//2, (y - 1)%t] and not visited[x, (y-1)%t]:
                         y -= 1
                     # left
-                    elif bond[x//2, y] and not visited[(x-1)%t, y]:
+                    elif bond[x//2, y] and not visited[(x-1)%n, y]:
                         x -= 1
                     # bottom
                     elif not bond[x//2, y] and not visited[x, (y+1)%t]:
                         y += 1
                     # right
-                    elif bond[x//2, y-1] and not visited[(x+1)%t, y]:
+                    elif bond[x//2, (y - 1)%t] and not visited[(x+1)%n, y]:
                         x += 1
                     # closed loop
                     else:
                         break
                 elif x % 2 == 0 and y % 2 == 1:
                     # top
-                    if not bond[x//2, y - 1] and not visited[x, (y-1)%t]:
+                    if not bond[x//2, (y - 1)%t] and not visited[x, (y-1)%t]:
                         y -= 1
                     # left
-                    elif bond[(x//2 - 1)%(t//2), y] and not visited[(x-1)%t, y]:
+                    elif bond[(x//2 - 1)%(n//2), y] and not visited[(x-1)%n, y]:
                         x -= 1
                     # bottom
-                    elif not bond[(x//2 - 1)%(t//2), y] and not visited[x, (y+1)%t]:
+                    elif not bond[(x//2 - 1)%(n//2), y] and not visited[x, (y+1)%t]:
                         y += 1
                     # right
-                    elif bond[x//2, y-1] and not visited[(x+1)%t, y]:
+                    elif bond[x//2, (y - 1)%t] and not visited[(x+1)%n, y]:
                         x += 1
                     # closed loop
                     else:
                         break
                 elif x % 2 == 1 and y % 2 == 1:
                     # top
-                    if not bond[x//2, y - 1] and not visited[x, (y-1)%t]:
+                    if not bond[x//2, (y - 1)%t] and not visited[x, (y-1)%t]:
                         y -= 1
                     # left
-                    elif bond[x//2, y - 1] and not visited[(x-1)%t, y]:
+                    elif bond[x//2, (y - 1)%t] and not visited[(x-1)%n, y]:
                         x -= 1
                     # bottom
                     elif not bond[x//2, y] and not visited[x, (y+1)%t]:
                         y += 1
                     # right
-                    elif bond[x//2, y] and not visited[(x+1)%t, y]:
+                    elif bond[x//2, y] and not visited[(x+1)%n, y]:
                         x += 1
                     # closed loop
                     else:
@@ -102,14 +105,21 @@ def main():
             if full:
                 break
 
-        # cluster assignment
-        for x, y in product(range(0, n, 2), range(t)):
-            if fermion[x, y] == fermion[x, (y + 1) % t] == fermion[(x + 1) % n, y] == fermion[(x + 1) % n, (y + 1) % t]:
+        # bond assignment
+        for x, y in product(range(n), range(t)):
+            if y % 2 != x % 2:
+                continue
+            if fermion[x, y] == fermion[(x + 1) % n, y] and fermion[x, (y + 1) % t] == fermion[(x + 1) % n, (y + 1) % t]:
                 bond[x // 2, y] = False
-            if fermion[x, y] != fermion[x, (y + 1) % t]:
+            elif fermion[x, y] != fermion[x, (y + 1) % t]:
                 bond[x// 2, y] = True
             else:
                 bond[x // 2, y] = False if random.random() < w_a/(w_a + w_b) else True
+            # calculate bond config in nicer lattice for debugging purposes
+            if y % 2 == 0:
+                bond_debug[x, y] = bond[x//2, y]
+            else:
+                bond_debug[x, y] = bond[x//2, y]
 
         # calculate weights
         weight_factor = 1
